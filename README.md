a97b8a73b62ca2078fe01dc966d2a829457993f2
Xavier S <spartan9077@gmail.com>
2023-04-16 15:21:05 -0600

    added user input + fixed false negative

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 # Usage
 
+If you want to try your own string and substring:
+python fsa_generator.py originalstring substring
+
+If you want to use the default string and substring:
 python fsa_generator.py
 
+for example:
+python fsa_generator.py thisisatest isa
+
 # Output
+
 The output will be written to a .yaml file in the same directory as the program.  
 Copy and paste the contents of the .yaml file into turingmachine.io
 
@@ -13,7 +21,6 @@ Copy and paste the contents of the .yaml file into turingmachine.io
 This is an extra credit effort for the homework, approved by the professor:
 ![image](https://user-images.githubusercontent.com/26730202/232341282-4b96cc40-ae15-46ef-a1fa-39008007c1c5.png)
 
-
 Extra credit portion of homework 2, Chapter 4 Automata, Section 1 Finite Automata, Question 6:
 Develop an algorithm to design finite automata for substring searching.
 The input should be the substring and the output is a finite automaton state
@@ -21,4 +28,5 @@ table. Be sure to specify your data structures and intuitively describe the
 algorithm.
 
 # Requirements
+
 python 3.6+
--- fsa_generator.py
+++ fsa_generator.py
@@ -1,3 +1,5 @@
+# used to capture user input
+import sys
 # only used for alphabet
 import string
 
@@ -25,17 +27,32 @@ def main(original, search):
     for index, char in enumerate(sub_str):
 
         # generate list of values that return us to start
-        alpha_copy = [letter for letter in alpha if letter != char]
+        # check if char is the same as the first letter of the substring
+        if char == sub_str[0]:
+            same_as_char = True
+            alpha_copy = [letter for letter in alpha if letter != char]
+
+        else:
+            same_as_char = False
+            alpha_copy = [letter for letter in alpha if letter !=
+                          char and letter != sub_str[0]]
+
         alpha_copy = ','.join(alpha_copy)
 
         yaml_output += f" S{index}:\n"
 
         yaml_output += f"  [{alpha_copy}] : " + "{R: S0}\n"
-        if index != len(sub_str) - 1:
-            yaml_output += f"  {char} : " + "{R: S"+str(index+1)+"}\n"
+
+        if not same_as_char:
+            if index == len(sub_str) - 1:
+                yaml_output += f"  {char} : " + "{R: Done}\n"
+
+            else:
+                yaml_output += f"  {char} : " + "{R: S"+str(index+1)+"}\n"
+            yaml_output += f"  {sub_str[0]} : " + "{R: S1}\n"
 
         else:
-            yaml_output += f"  {char} : " + "{R: Done}\n"
+            yaml_output += f"  {char} : " + "{R: S"+str(index+1)+"}\n"
 
     alpha = ",".join(alpha)
     yaml_output += f" Done:\n  [{alpha}] : " + "R\n"
@@ -45,4 +62,9 @@ def main(original, search):
 
 
 if __name__ == '__main__':
-    main('pythonisprettygreatngl', 'great')
+
+    # support user input
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        main('pythonisprettygreatngl', 'great')
